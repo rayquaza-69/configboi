0f222b2381eee3110a6aa97f614a5b48f06060a0
rayquaza-69 <85679614+rayquaza-69@users.noreply.github.com>
2022-01-03 19:38:54 +0530

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,29 @@
+set -x EDITOR vim
+echo
+neofetch
+
+function lsa
+  ls -lah
+end
+
+
+function fish_greeting
+end
+
+
+function yesorno
+  random choice "yes" "no"
+end
+
+function a
+  clear
+end
+
+function aa
+  cd ~
+  clear
+end
+
+
+python ~/schedule.py
+
